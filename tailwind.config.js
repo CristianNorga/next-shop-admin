25c5475d130a6319efb922e3d8d74828fcfbc37f
Cristian Noreña <cristian.rraga95@hotmail.com>
2022-11-30 17:29:44 -0500

    integramos componentes predefinidos con TailwindUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 const colors = require('tailwindcss/colors');
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
 
 module.exports = {
   content: ['./src/**/*{html,js}'],
